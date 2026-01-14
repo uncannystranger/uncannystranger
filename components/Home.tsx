14f9eb5562f62d444bad00bdc5561a4c1b258fe1
Abdullahi Mohamud <103581520+uncannystranger@users.noreply.github.com>
2026-01-14 22:21:36 +0300

    Update Home.tsx

--- components/Home.tsx
+++ components/Home.tsx
@@ -19,7 +19,7 @@ interface HomeProps {
 }
 
 const HERO_IMAGE =
-  "https://res.cloudinary.com/duwhuzkib/image/upload/v1768417940/_DSC9555.ARW_fm87ao.png";
+  "https://res.cloudinary.com/duwhuzkib/image/upload/v1768418457/abdullahi-maxamed-LyklJe8ufj8-unsplash_1_smbjcu.jpg";
 
 const Home = ({ setSection }: HomeProps) => {
   const shouldReduceMotion = useReducedMotion();
